f26a4cb8032c748d279eb0d82e2dc77935d47dcd
charanveers <example@example.com>
2019-11-15 12:18:37 +0530

    changes by chran

--- app/src/main/java/com/smartitventures/testnew/MainActivity.java
+++ app/src/main/java/com/smartitventures/testnew/MainActivity.java
@@ -1,11 +1,23 @@
 package com.smartitventures.testnew;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.smartitventures.mylibrary.CardDetailsActivity;
+import com.smartitventures.mylibrary.IntroActivity;
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myintent = new Intent(MainActivity.this, CardDetailsActivity.class);
+                startActivity(myintent);
+            }
+        });
     }
 }
--- mylibrary/src/main/java/com/smartitventures/mylibrary/Display.java
+++
@@ -1,9 +0,0 @@
-package com.smartitventures.mylibrary;
-public class Display {
-
-    public static void name(String name)
-    {
-        System.out.println("Name= "+name);
-
-    }
-}
