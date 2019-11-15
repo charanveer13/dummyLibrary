428ac163074fc69d587ac70a9e9ff5b7856a7023
charanveers <example@example.com>
2019-11-15 13:51:56 +0530

    changes by chran third commint

---
+++ test/src/main/java/com/smartitventures/test/ScanCard.java
@@ -0,0 +1,9 @@
+package com.smartitventures.test;
+import android.app.Activity;
+import com.getbouncer.cardscan.ScanActivity;
+
+public class ScanCard {
+    public ScanCard(Activity context) {
+        ScanActivity.start(context);
+    }
+}
